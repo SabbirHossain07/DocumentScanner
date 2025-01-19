1b34acbee9d85f47d58add0f0209b1f2d67e726e
Sabbir Hossain <sabbirsopnil07@gmail.com>
2025-01-19 13:55:29 +0600

    Initial Commit

---
+++ DocumentScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DocumentScanner
+//
+//  Created by Sopnil Sohan on 19/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DocumentScanner/DocumentScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentScannerApp.swift
+//  DocumentScanner
+//
+//  Created by Sopnil Sohan on 19/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DocumentScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
